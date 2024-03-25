fd522a3849bd57bd38a3e49d5f8caaa697ee47c7
Jakeban17 <jnielsen1@ycp.edu>
2024-03-25 17:01:07 -0400

    Base controller

---
+++ RevMetrixUI/src/revMetrix/controller/GameController.java
@@ -0,0 +1,16 @@
+package revMetrix.controller;
+
+public class GameController {
+	public void addShot() {
+		return;
+	}
+	public void findScore() {
+		return;
+	}
+	public void incrementShot() {
+		return;
+	}
+	public void deincrementShot() {
+		return;
+	}
+}
