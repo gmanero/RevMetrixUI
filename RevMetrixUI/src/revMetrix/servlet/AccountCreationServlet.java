85da85ac0e8996b02de44b7f9bbe9dee913bf5b6
TKPRyon <rwashington1@ycp.edu>
2024-03-25 18:43:38 -0400

    Changes to ACServlet and model

--- RevMetrixUI/src/revMetrix/servlet/AccountCreationServlet.java
+++ RevMetrixUI/src/revMetrix/servlet/AccountCreationServlet.java
@@ -8,11 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import revMetrix.model.RevMetrix;
-import revMetrix.model.RevMetrix.Account;
-import java.util.ArrayList;  
-import java.util.Iterator;  
-import java.util.List;  
-import java.util.ListIterator;  
+import revMetrix.model.RevMetrix.Account; 
 
 public class AccountCreationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
