a4e1aba27435f0d6eb843081aff0a2b187c02580
Jakeban17 <jnielsen1@ycp.edu>
2024-05-03 16:35:27 -0400

    Fix log out button turns into Log in immediately upon log out

--- RevMetrixUI/src/revMetrix/servlet/IndexServlet.java
+++ RevMetrixUI/src/revMetrix/servlet/IndexServlet.java
@@ -1,7 +1,6 @@
 package revMetrix.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import revMetrix.controller.AllAccountsController;
-import revMetrix.controller.GameController;
-import revMetrix.db.model.Ball;
 import revMetrix.model.RevMetrix;
 
 public class IndexServlet extends HttpServlet {
@@ -25,85 +22,62 @@ public class IndexServlet extends HttpServlet {
         String destIn = req.getParameter("destination");
         String destOut = null;
         String loggedInName = "";
-        
+        boolean loggedIn = false; // Initialize loggedIn to false
 
         AllAccountsController controller = new AllAccountsController();
-        boolean loggedIn = controller.isLoggedInAccount();
+        loggedIn = controller.isLoggedInAccount();
 
-        
-        if (!loggedIn) {
-        	if("accountCreation".equals(destIn)) {
-                destOut = "/_view/accountCreation.jsp";
-            }
-        	else {
-        		destOut = "/_view/login.jsp";
-        	}
+        if (loggedIn) {
+            loggedInName = "Hello, " + controller.findLoggedInUser();
+            System.out.println("Logged in name: " + loggedInName);
         }
-        else {
-        	loggedInName = "Hello, " + controller.findLoggedInUser();
-        	System.out.println("Logged in name: " + loggedInName);
-        	// Forward based on the action parameter
+
+        // Handle logout action
+        if ("logout".equals(destIn)) {
+            controller.logOutAllAccounts();
+            loggedInName = "";
+            loggedIn = false; // Update loggedIn to false after logout
+            destOut = "/_view/login.jsp"; // Redirect to login page after logout
+        } else {
+            // Handle other actions
             if ("RevMetrix".equals(destIn)) {
-               destOut = "/_view/index.jsp";
-            } 
-            else if ("login".equals(destIn)) {
-                 destOut = "/_view/login.jsp";
-            } 
-            else if ("logout".equals(destIn)) {
+                destOut = "/_view/index.jsp";
+            } else if ("login".equals(destIn)) {
                 destOut = "/_view/login.jsp";
-                controller.logOutAllAccounts();
-                loggedInName = "";
-           } 
-            else if ("event".equals(destIn)) {
-                 destOut = "/_view/event.jsp";
-            } 
-            else if ("register".equals(destIn)) {
+            } else if ("event".equals(destIn)) {
+                destOut = "/_view/event.jsp";
+            } else if ("register".equals(destIn)) {
                 destOut = "/_view/registration.jsp";
-            } 
-            else if ("account".equals(destIn)) {
+            } else if ("account".equals(destIn)) {
                 destOut = "/_view/account.jsp";
-            }  
-            else if ("game".equals(destIn)) {
+            } else if ("game".equals(destIn)) {
                 destOut = "/game";
-                //req.getRequestDispatcher("/game").forward(req, resp);
-            } 
-            else if ("stats".equals(destIn)) {
+            } else if ("stats".equals(destIn)) {
                 destOut = "/_view/stats.jsp";    
-            } 
-            else if ("create".equals(destIn)) {
+            } else if ("create".equals(destIn)) {
                 destOut = "/_view/create.jsp";
-            } 
-            else if ("createLeague".equals(destIn)) {
+            } else if ("createLeague".equals(destIn)) {
                 destOut = "/_view/createLeague.jsp";
-            }
-            else if ("createPractice".equals(destIn)) {
+            } else if ("createPractice".equals(destIn)) {
                 destOut = "/_view/createPractice.jsp";
-            }
-            else if ("createTournament".equals(destIn)) {
+            } else if ("createTournament".equals(destIn)) {
                 destOut = "/_view/createTournament.jsp";
-            }
-            else if ("leagues".equals(destIn)) {
+            } else if ("leagues".equals(destIn)) {
                 destOut = "/_view/leagues.jsp";
-            }
-            else if ("practices".equals(destIn)) {
+            } else if ("practices".equals(destIn)) {
                 destOut = "/_view/practices.jsp";
-            }
-            else if ("tournaments".equals(destIn)) {
+            } else if ("tournaments".equals(destIn)) {
                 destOut = "/_view/tournaments.jsp";
-            }
-            else if ("yourGames".equals(destIn)) {
+            } else if ("yourGames".equals(destIn)) {
                 destOut = "/_view/yourGames.jsp";
-            }
-            else {
-               destOut = "/_view/index.jsp";
+            } else {
+                destOut = "/_view/index.jsp";
             }
         }
-        
 
+        // Set attributes and forward the request
         req.setAttribute("loggedInName", loggedInName);
         req.setAttribute("loggedIn", loggedIn);
-
-        // Forward the request after setting destOut
         req.getRequestDispatcher(destOut).forward(req, resp);
     }
 }
