1ddd6007707422971ecfd52ccb3c291e658070db
Jakeban17 <jnielsen1@ycp.edu>
2024-05-03 14:44:08 -0400

    Forces you to log in if there is no one logged in

--- RevMetrixUI/src/revMetrix/servlet/IndexServlet.java
+++ RevMetrixUI/src/revMetrix/servlet/IndexServlet.java
@@ -35,64 +35,71 @@ public class IndexServlet extends HttpServlet {
         	System.out.println("Logged in name: " + loggedInName);
         }
 
-        // Forward based on the action parameter
-        if ("RevMetrix".equals(destIn)) {
-           destOut = "/_view/index.jsp";
-        } 
-        else if ("login".equals(destIn)) {
-             destOut = "/_view/login.jsp";
-        } 
-        else if ("logout".equals(destIn)) {
-            destOut = "/_view/login.jsp";
-            controller.logOutAllAccounts();
-            loggedInName = "";
-       } 
-        else if ("event".equals(destIn)) {
-             destOut = "/_view/event.jsp";
-        } 
-        else if ("register".equals(destIn)) {
-            destOut = "/_view/registration.jsp";
-        } 
-        else if ("account".equals(destIn)) {
-            destOut = "/_view/account.jsp";
-        }  
-        else if ("game".equals(destIn)) {
-            destOut = "/game";
-            //req.getRequestDispatcher("/game").forward(req, resp);
-        } 
-        else if ("stats".equals(destIn)) {
-            destOut = "/_view/stats.jsp";    
-        } 
-        else if ("create".equals(destIn)) {
-            destOut = "/_view/create.jsp";
-        } 
-        else if ("createLeague".equals(destIn)) {
-            destOut = "/_view/createLeague.jsp";
-        }
-        else if ("createPractice".equals(destIn)) {
-            destOut = "/_view/createPractice.jsp";
-        }
-        else if ("createTournament".equals(destIn)) {
-            destOut = "/_view/createTournament.jsp";
-        }
-        else if ("leagues".equals(destIn)) {
-            destOut = "/_view/leagues.jsp";
-        }
-        else if ("practices".equals(destIn)) {
-            destOut = "/_view/practices.jsp";
-        }
-        else if ("tournaments".equals(destIn)) {
-            destOut = "/_view/tournaments.jsp";
-        }
-        else if ("yourGames".equals(destIn)) {
-            destOut = "/_view/yourGames.jsp";
-        }
-        else if ("accountCreation".equals(destIn)) {
-            destOut = "/_view/accountCreation.jsp";
+        
+        if (!loggedIn) {
+        	destOut = "/_view/login.jsp";
         }
         else {
-           destOut = "/_view/index.jsp";
+        	// Forward based on the action parameter
+            if ("RevMetrix".equals(destIn)) {
+               destOut = "/_view/index.jsp";
+            } 
+            else if ("login".equals(destIn)) {
+                 destOut = "/_view/login.jsp";
+            } 
+            else if ("logout".equals(destIn)) {
+                destOut = "/_view/login.jsp";
+                controller.logOutAllAccounts();
+                loggedInName = "";
+           } 
+            else if ("event".equals(destIn)) {
+                 destOut = "/_view/event.jsp";
+            } 
+            else if ("register".equals(destIn)) {
+                destOut = "/_view/registration.jsp";
+            } 
+            else if ("account".equals(destIn)) {
+                destOut = "/_view/account.jsp";
+            }  
+            else if ("game".equals(destIn)) {
+                destOut = "/game";
+                //req.getRequestDispatcher("/game").forward(req, resp);
+            } 
+            else if ("stats".equals(destIn)) {
+                destOut = "/_view/stats.jsp";    
+            } 
+            else if ("create".equals(destIn)) {
+                destOut = "/_view/create.jsp";
+            } 
+            else if ("createLeague".equals(destIn)) {
+                destOut = "/_view/createLeague.jsp";
+            }
+            else if ("createPractice".equals(destIn)) {
+                destOut = "/_view/createPractice.jsp";
+            }
+            else if ("createTournament".equals(destIn)) {
+                destOut = "/_view/createTournament.jsp";
+            }
+            else if ("leagues".equals(destIn)) {
+                destOut = "/_view/leagues.jsp";
+            }
+            else if ("practices".equals(destIn)) {
+                destOut = "/_view/practices.jsp";
+            }
+            else if ("tournaments".equals(destIn)) {
+                destOut = "/_view/tournaments.jsp";
+            }
+            else if ("yourGames".equals(destIn)) {
+                destOut = "/_view/yourGames.jsp";
+            }
+            else if ("accountCreation".equals(destIn)) {
+                destOut = "/_view/accountCreation.jsp";
+            }
+            else {
+               destOut = "/_view/index.jsp";
+            }
         }
+        
 
         req.setAttribute("loggedInName", loggedInName);
         req.setAttribute("loggedIn", loggedIn);
